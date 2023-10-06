ca69e761d682fc04bc67e51b93ce7968ca040788
omri yakir <omriyakir@mail.tau.ac.il>
2023-10-06 14:04:34 +0300

    update file

--- protein_classification/download_entries.py
+++ protein_classification/download_entries.py
@@ -1,15 +1,16 @@
 import sys,os
 sys.path.append(os.getcwd())
 sys.path.append('../ScanNet_Ub/')
+sys.path.append(r'C:\Users\omriy\UBDAndScanNet\ScanNet_Ub')
 import pandas as pd
-from preprocessing import PDBio
+from ScanNet_Ub.preprocessing import PDBio
 import numpy as np
 download_batch = 10
 try:
     batch = int(sys.argv[1])
 except:
     batch = 0
-table = pd.read_csv('protein_classification/uniprotnamecsCSV.csv')
+table = pd.read_csv(r'C:\Users\omriy\UBDAndScanNet\newUBD\UBDModel\protein_classification\uniprotnamecsCSV.csv')
 all_examples = []
 for column in table.columns[:-1]:
     all_examples += list(table[column].dropna())
@@ -20,3 +21,4 @@ batch_examples = all_examples[batch_size*batch: batch_size * (batch+1)]
 print(batch_examples)
 pdblist = PDBio.myPDBList()
 pdblist.download_pdb_files(batch_examples)
+print(1)
