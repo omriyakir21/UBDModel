73cdcb1934beab381204afef5aee53c2d08acf4d
omri yakir <omriyakir@mail.tau.ac.il>
2024-03-31 13:28:41 +0300

    training stoppage with evolution

--- aggregateScoringMLP4_3.py
+++ aggregateScoringMLP4_3.py
@@ -11,7 +11,7 @@ allInfoDicts = utils.loadPickle(
     os.path.join(path.aggregateFunctionMLPDir, os.path.join('dataForTraining23_3', 'allInfoDicts.pkl')))
 dictsForTraining = utils.loadPickle(
     os.path.join(path.aggregateFunctionMLPDir, os.path.join('dataForTraining23_3', 'dictsForTraining.pkl')))
-directory_name = sys.argv[3]
+directory_name = '/home/iscb/wolfson/omriyakir/UBDModel/aggregateFunctionMLP/MLP_evolution_trainingAccuracyStoppage/'
 
 
 allArchitecturesAucs = []
