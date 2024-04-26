98a51a027022a2d81e9950a33c3ba9ae8e51390c
omri yakir <omriyakir@mail.tau.ac.il>
2024-04-26 11:27:33 +0300

    half predictions change

--- concatPklAggregateScoringMLPGridSearch.py
+++ concatPklAggregateScoringMLPGridSearch.py
@@ -26,29 +26,27 @@ def concatAllFilesOfName(dirPath, name):
 
 
 def concatAllHalfPredictions(dirPath):
-    dictsDict = {}
+    predictionsDict = {}
     for filename in os.listdir(dirPath):
         if ' m_c' in filename:
             prefix = filename.split(' m_c')[0]
             predictions = loadPickle(os.path.join(dirPath, filename))
-            if prefix not in dictsDict:
-                dictsDict[prefix] = predictions
+            if prefix not in predictionsDict:
+                predictionsDict[prefix] = predictions
             else:
-                dictsDict[prefix].update(predictions)
-    for key in dictsDict:
+                predictionsDict[prefix] += predictions
+    for key in predictionsDict:
         filename = key.split('m_a')[0]
-        saveAsPickle(dictsDict[key], os.path.join(dirPath, filename))
+        saveAsPickle(predictionsDict[key], os.path.join(dirPath, filename))
 
 
 dirPath = '/home/iscb/wolfson/omriyakir/UBDModel/aggregateFunctionMLP/MLP_MSA_val_AUC_stoppage_with_evolution_85_plddt_all_organizems_15_4/'
 name = 'Allarchite'
 
-
-concatenated_lists= concatAllFilesOfName(dirPath,name)
-saveAsPickle(concatenated_lists, os.path.join(dirPath,'allArchitectureAucs'))
+concatenated_lists = concatAllFilesOfName(dirPath, name)
+saveAsPickle(concatenated_lists, os.path.join(dirPath, 'allArchitectureAucs'))
 name = 'totalA'
-concatenated_lists= concatAllFilesOfName(dirPath,name)
-saveAsPickle(concatenated_lists, os.path.join(dirPath,'totalAucs'))
+concatenated_lists = concatAllFilesOfName(dirPath, name)
+saveAsPickle(concatenated_lists, os.path.join(dirPath, 'totalAucs'))
 
 concatAllHalfPredictions(dirPath)
-
