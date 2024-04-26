37e0ab808c5d1932da70d65adf407b44083e5e8d
omri yakir <omriyakir@mail.tau.ac.il>
2024-04-26 12:05:05 +0300

    change for fileformat

--- aggregateScoringMLP4_3.py
+++ aggregateScoringMLP4_3.py
@@ -109,4 +109,4 @@ utils.saveAsPickle(allArchitecturesAucs,
                    os.path.join(directory_name, 'allArchitecturesAucs' + str(n_layers) + "m_a: " + str(m_a)+" m_c: "+str(m_c)))
 utils.saveAsPickle(totalAucs, os.path.join(directory_name, 'totalAucs' + str(n_layers) + "m_a: " + str(m_a)+" m_c: "+str(m_c)))
 utils.saveAsPickle(allArchitecturesPredictionsAndLabels,
-                   os.path.join(directory_name, 'predictions_labels_' + "m_a: " + str(m_a)+" m_c: "+str(m_c)))
+                   os.path.join(directory_name, 'predictions_labels_' + str(n_layers)+ "m_a: " + str(m_a)+" m_c: "+str(m_c)))
