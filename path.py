60ecb814951105f70b50d1aa49ef534aa1a8ae61
omri yakir <omriyakir@mail.tau.ac.il>
2024-05-02 15:50:26 +0300

    path update

--- path.py
+++ path.py
@@ -5,9 +5,19 @@ if linux:
     GoPath = '/home/iscb/wolfson/omriyakir/UBDModel/GO'
     predictionsToDataSetDir = '/home/iscb/wolfson/omriyakir/UBDModel/predictionsToDataSet'
     ScanNetPredictionsPath = '/home/iscb/wolfson/omriyakir/UBDModel/model_predictions/'
+    gridSearchDir = ('/home/iscb/wolfson/omriyakir/UBDModel/aggregateFunctionMLP'
+                     '/MLP_MSA_val_AUC_stoppage_with_evolution_50_plddt_all_organizems_15_4')
+    trainingDir = ('/home/iscb/wolfson/omriyakir/UBDModel/predictionsToDataSet'
+                   '/with_evolution_50_plddt_all_organizems_15_4/trainingDicts/')
+
 else:
     mainProjectDir = r'C:\Users\omriy\UBDAndScanNet\newUBD\UBDModel'
     aggregateFunctionMLPDir = r'C:\Users\omriy\UBDAndScanNet\newUBD\UBDModel\aggregateFunctionMLP'
     GoPath = r'C:\Users\omriy\UBDAndScanNet\newUBD\UBDModel\GO'
     predictionsToDataSetDir = r'C:\Users\omriy\UBDAndScanNet\newUBD\UBDModel\predictionsToDataSetDir'
     ScanNetPredictionsPath = r'C:\Users\omriy\UBDAndScanNet\newUBD\UBDModel\Predictions'
+    gridSearchDir = (
+        r'C:\Users\omriy\UBDAndScanNet\newUBD\UBDModel\aggregateFunctionMLP'
+        r'\MLP_MSA_val_AUC_stoppage_with_evolution_50_plddt_all_organizems_15_4')
+    trainingDir = (r'C:\Users\omriy\UBDAndScanNet\newUBD\UBDModel\predictionsToDataSet'
+                   r'\with_evolution_50_plddt_all_organizems_15_4\trainingDicts')
