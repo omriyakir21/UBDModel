4a0977779b196f0ebd420728a36336615e473af9
omri yakir <omriyakir@mail.tau.ac.il>
2024-06-17 18:40:34 +0300

    Af3 iptm file

--- AF3_iptm_investigation.py
+++ AF3_iptm_investigation.py
@@ -37,7 +37,8 @@ def select_n_random_indices(n):
     return selected_keys
 
 
-def createInputFiles(selected_keys):
+def createInputFiles(n):
+    selected_keys = utils.loadPickle(os.path.join(path.AF2_multimerDir, 'selected_keys_'+str(n)))
     for uniprot_id in selected_keys:
         sequence = allPredictions['dict_sequences'][uniprot_id]
         fasta_content = f">{uniprot_id}\n{sequence}:\n{ubiqString}\n"
@@ -47,5 +48,6 @@ def createInputFiles(selected_keys):
             f.write(fasta_content)
 
 
-selected_keys = select_n_random_indices(200)
-utils.saveAsPickle(selected_keys, os.path.join(path.AF2_multimerDir, 'selected_keys_200'))
+# selected_keys = select_n_random_indices(200)
+# utils.saveAsPickle(selected_keys, os.path.join(path.AF2_multimerDir, 'selected_keys_200'))
+createInputFiles(200)
