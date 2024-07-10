a8a44651e4083ca358f7c39b2602b948e521c555
omri yakir <omriyakir@mail.tau.ac.il>
2024-07-10 14:03:06 +0300

    dali try another example

--- ClassifyExperimentalProteins.py
+++ ClassifyExperimentalProteins.py
@@ -71,12 +71,12 @@ def convert_cif_to_pdb_in_directory(assemblies_dir):
 # convert_cif_to_pdb_in_directory(os.path.join(path.experimentsDir, 'listOfProteins'))
 
 dali_alligner_object = dali_alligner.DaliAligner()
-ref_name = '1cmxB'
+ref_name = ProteinsToExperiment[0][:4]+'B'
 mov_name = '1cmxA'
 print(f'ref_name{ref_name}, mov_name{mov_name}')
 
-ref_path = find_pdb_path_with_subword(path.assembliesDir, mov_name[:4])
-mov_path = find_pdb_path_with_subword(os.path.join(path.experimentsDir, 'listOfProteins'), ref_name[:4])
+mov_path = find_pdb_path_with_subword(path.assembliesDir, mov_name[:4])
+ref_path = find_pdb_path_with_subword(os.path.join(path.experimentsDir, 'listOfProteins'), ref_name[:4])
 print(f'refPath{ref_path}, movPath{mov_path}')
 
 resultsDict = {}
