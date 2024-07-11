b3d256c8196a208fdf3bf35f9b9e4fe551a3b5c4
omri yakir <omriyakir@mail.tau.ac.il>
2024-07-11 12:49:20 +0300

    asf

--- ClassifyExperimentalProteins.py
+++ ClassifyExperimentalProteins.py
@@ -71,12 +71,16 @@ def convert_cif_to_pdb_in_directory(assemblies_dir):
 # convert_cif_to_pdb_in_directory(os.path.join(path.experimentsDir, 'listOfProteins'))
 
 dali_alligner_object = dali_alligner.DaliAligner()
-ref_name = ProteinsToExperiment[0][:4]+'B'
-mov_name = '1cmxA'
+# ref_name = ProteinsToExperiment[0][:4]+'B'
+ref_name = '3lcuA'
+mov_name = '3lbyA'
+mov_path = "/home/iscb/wolfson/omriyakir/DaliLite.v5/3lby.pdb"
+ref_path = "/home/iscb/wolfson/omriyakir/DaliLite.v5/3lcu.pdb"
+# mov_name = '1cmxA'
 print(f'ref_name{ref_name}, mov_name{mov_name}')
 
-mov_path = find_pdb_path_with_subword(path.assembliesDir, mov_name[:4])
-ref_path = find_pdb_path_with_subword(os.path.join(path.experimentsDir, 'listOfProteins'), ref_name[:4])
+# mov_path = find_pdb_path_with_subword(path.assembliesDir, mov_name[:4])
+# ref_path = find_pdb_path_with_subword(os.path.join(path.experimentsDir, 'listOfProteins'), ref_name[:4])
 print(f'refPath{ref_path}, movPath{mov_path}')
 
 resultsDict = {}
